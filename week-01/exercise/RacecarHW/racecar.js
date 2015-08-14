5ea9d383d94260357d7ee9a1b6f0c1d10634641a
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2015-08-14 16:57:19 -0700

    racecarHW

---
+++ week-01/exercise/RacecarHW/racecar.js
@@ -0,0 +1,17 @@
+console.log("Sanity Check: JS is working!");
+
+$(document).ready(function() {
+
+	$('img#car1').attr('src');
+
+		$('') // need to add a loop perhaps? to cycle through the TD's????. Then not sure whether need to use .HTML() or .ATTR()
+
+
+	$
+
+
+
+});
+
+
+
