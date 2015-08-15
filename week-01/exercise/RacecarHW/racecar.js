ff988165196828dea92eece1c0b0416c039fa8e5
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2015-08-15 13:50:45 -0700

    remove car1 on A keypress

--- week-01/exercise/RacecarHW/racecar.js
+++ week-01/exercise/RacecarHW/racecar.js
@@ -1,17 +1,91 @@
 console.log("Sanity Check: JS is working!");
 
+	var car1 = 'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png';
+	var car2 = 'http://www.ban-boredom.com/mini-car.gif';
+	var nextCell = $(this).closest('td').next();
+
 $(document).ready(function() {
 
-	$('img#car1').attr('src');
+// var a = jQuery.Event("keypress");
 
-		$('') // need to add a loop perhaps? to cycle through the TD's????. Then not sure whether need to use .HTML() or .ATTR()
-
-
-	$
+// var e.which = 65;
+// var e.which = 65;
 
 
-
+	$(document).keydown(function(e) {
+	    if(e.keyCode == 65){
+	        console.log('A was pressed');
+	        $('#car1').remove() ;
+			// $('#car1').append(nextCell);
+			$(nextCell).attr("<img class='car' id='car1' src=" + 'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png' + ">");
+	    }
+	});
 });
+
+
+
+// 	$(document).on("keypress", function(e) {
+//   		if(e.keyCode == 65){
+// 	        console.log('A was pressed');
+// 	        $('img#car1.car').empty() ;
+// 			// $('#car1').append(nextCell);
+// 			$(nextCell).attr("<img class='car' id='car1' src=" + 'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png' + ">");
+//  	};
+// });
+
+
+// 	$(document).keydown(function(e) {
+// 	    if(e.keyCode == 65){
+// 	        console.log('A was pressed');
+// 	        $('img#car1.car').empty() ;
+// 			// $('#car1').append(nextCell);
+// 			$(nextCell).attr("<img class='car' id='car1' src=" + 'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png' + ">");
+// 	    }
+// 	});
+// });
+
+
+
+
+
+// $('img#car1.car').keypress(function(e) {
+// 		console.log("tested OK step1");
+// 		if(e.which === 65) {
+// 			console.log('tested OK step2');
+// 			$('#car1').empty() ;
+// 			$('#car1').append(nextCell);
+// 			//$(nextCell).attr("<img class='car' id='car1' src=" + 'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png' + ">");
+// 		}
+// 	});
+
+
+	
+// WORKING CODE - acknowledges key pressed
+
+// 	$(document).keydown(function(e) {
+// 	    if(e.keyCode == 65){
+// 	        console.log('A was pressed');
+// 	        // $('img#car1.car').empty() ;
+// 			// $('#car1').append(nextCell);
+// 			$(nextCell).attr("<img class='car' id='car1' src=" + 'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png' + ">");
+// 	    }
+// 	});
+// });
+
+
+
+
+
+	
+
+//		$('') // need to add a loop perhaps? to cycle through the TD's????. Then not sure whether need to use .HTML() or .ATTR()
+
+
+//	$
+
+
+
+
 
 
 
