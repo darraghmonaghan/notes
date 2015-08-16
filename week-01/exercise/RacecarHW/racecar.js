e2766057d20c6791b644eb0d7496817a909c94b1
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2015-08-16 16:08:23 -0700

    semi working game, CSS needs adjustment

--- week-01/exercise/RacecarHW/racecar.js
+++ week-01/exercise/RacecarHW/racecar.js
@@ -21,105 +21,55 @@ $(document).ready(function() {
 		currentClickCar2 = 0;
 	}
 
-	function winner() {
+	function calculateWinner() {
 		if (currentClickCar1 >= clicksToWin) {
 	   		car1Score ++;
 	    	$('td#car1Score').text(car1Score);
-	    	// function for showing winner
+	    	copsWin();
 	    	gameReset();
-
 		} else if (currentClickCar2 >= clicksToWin) {
 	  		car2Score ++;
 			$('td#car2Score').text(car2Score);
-			
-			// function for showing winner
+			robbersWin();
 			gameReset();
 		}
 	}
 
+	function copsWin() {
+		$('#copsWin').toggle(1000);
+		$('#copsWin').toggle(1000);
+
+	}
+
+	function robbersWin() {
+		$('#robbersWin').toggle(1000);
+		$('#robbersWin').toggle(1000);
+	}
+
+
 		$(document).keydown(function(e) {
+
 		    if(e.keyCode == 65){
 		        console.log('A was pressed') ;
 				$('#car1').remove() ;
 				currentClickCar1++;
 				$('tr.1').append("<td class='newTD'><img id='car1' src=" + car1 + "></td>");       // this works.......
-				winner();
+				calculateWinner();
+				// showWinner();
 
 		    } else if (e.keyCode == 76) {
 		        console.log('L was pressed');
 		        $('#car2').remove() ;
 		        currentClickCar2++;
 		        $('tr.2').append("<td class='newTD'><img id='car2' src=" + car2 + "></td>");       // this works.......  
-			   	winner();
+			   	calculateWinner();
+			   	// showWinner();
 		    }
 		});
 });
 
 
 
-
-
-
-
-
-// 	$(document).on("keypress", function(e) {
-//   		if(e.keyCode == 65){
-// 	        console.log('A was pressed');
-// 	        $('img#car1.car').empty() ;
-// 			// $('#car1').append(nextCell);
-// 			$(nextCell).attr("<img class='car' id='car1' src=" + 'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png' + ">");
-//  	};
-// });
-
-
-// 	$(document).keydown(function(e) {
-// 	    if(e.keyCode == 65){
-// 	        console.log('A was pressed');
-// 	        $('img#car1.car').empty() ;
-// 			// $('#car1').append(nextCell);
-// 			$(nextCell).attr("<img class='car' id='car1' src=" + 'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png' + ">");
-// 	    }
-// 	});
-// });
-
-
-
-
-
-// $('img#car1.car').keypress(function(e) {
-// 		console.log("tested OK step1");
-// 		if(e.which === 65) {
-// 			console.log('tested OK step2');
-// 			$('#car1').empty() ;
-// 			$('#car1').append(nextCell);
-// 			//$(nextCell).attr("<img class='car' id='car1' src=" + 'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png' + ">");
-// 		}
-// 	});
-
-
-	
-// WORKING CODE - acknowledges key pressed
-
-// 	$(document).keydown(function(e) {
-// 	    if(e.keyCode == 65){
-// 	        console.log('A was pressed');
-// 	        // $('img#car1.car').empty() ;
-// 			// $('#car1').append(nextCell);
-// 			$(nextCell).attr("<img class='car' id='car1' src=" + 'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png' + ">");
-// 	    }
-// 	});
-// });
-
-
-
-
-
-	
-
-//		$('') // need to add a loop perhaps? to cycle through the TD's????. Then not sure whether need to use .HTML() or .ATTR()
-
-
-//	$
 
 
 
