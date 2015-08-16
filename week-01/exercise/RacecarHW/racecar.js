951dec68150d7394602425c7e4a7b4abca7e95b8
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2015-08-16 14:11:17 -0700

    working race plus win counter & reset

--- week-01/exercise/RacecarHW/racecar.js
+++ week-01/exercise/RacecarHW/racecar.js
@@ -1,66 +1,57 @@
+
 console.log("Sanity Check: JS is working!");
 
 	var car1 = 'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png';
 	var car2 = 'http://www.ban-boredom.com/mini-car.gif';
 	var nextCell = $(this).closest('td').next();
-//	var Tds = document.getElementById("tdCount");
-//	var	raceDuration = Tds.length;
 	var currentClickCar1 = 0;
 	var currentClickCar2 = 0;
 	var car1Score = 0;
 	var car2Score = 0;
 	var clicksToWin = 10;
-
-
-
+	var playTo = 3;
 
 $(document).ready(function() {
 
-function gameReset () {
-	// $('#car1').remove() ;
-	// $('#car2').remove() ;
-	// $('tr.1').append("<td class='newTD'><img id='car1' src=" + car1 + "></td>");
-	$('td.newTD').remove();
-	$('tr.1').append("<td class='newTD'><img id='car1' src=" + car1 + "></td>");
-	$('tr.2').append("<td class='newTD'><img id='car2' src=" + car2 + "></td>");
-	car1Score = 0;
-	car2Score = 0;
-}
+	function gameReset () {
+		$('td.newTD').remove();
+		$('tr.1').append("<td class='newTD'><img id='car1' src=" + car1 + "></td>");
+		$('tr.2').append("<td class='newTD'><img id='car2' src=" + car2 + "></td>");
+		currentClickCar1 = 0;
+		currentClickCar2 = 0;
+	}
 
-	$(document).keydown(function(e) {
-	    if(e.keyCode == 65){
-	        console.log('A was pressed') ;
-			$('#car1').remove() ;
-			currentClickCar1++;
-			        // $(this).parent().next().html('<p>hello</p>') ;
-			       	// $(this).next().html('<p>hello</p>') ;
-			        // $(nextCell).html('<p>hello</p>');
-			        // $('td').html('<p>hello</p>');
-					//$('#test').html("<img id='car1' src=" + "'http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png'" + ">");
-					// $('#test').prepend('<img id="car1" src="http://www.weprintfridgemagnets.co.uk/wp-content/uploads/2014/01/police-car.png">');
-					//$("tr.1").append("<td>New Column</td>");
-			$('tr.1').append("<td class='newTD'><img id='car1' src=" + car1 + "></td>");       // this works.......
-
-	    } else if (e.keyCode == 76) {
-	        console.log('L was pressed');
-	        $('#car2').remove() ;
-	        currentClickCar2++;
-	        $('tr.2').append("<td class='newTD'><img id='car2' src=" + car2 + "></td>");       // this works.......   
-	    }
-
-	    if (currentClickCar1 >= clicksToWin) {
-	    	car1Score ++;
+	function winner() {
+		if (currentClickCar1 >= clicksToWin) {
+	   		car1Score ++;
 	    	$('td#car1Score').text(car1Score);
 	    	// function for showing winner
 	    	gameReset();
 
-	    } else if (currentClickCar2 >= clicksToWin) {
-	    	car2Score ++;
-	    	$('td#car2Score').text(car2Score)
-	    	gameReset();
-	    }
+		} else if (currentClickCar2 >= clicksToWin) {
+	  		car2Score ++;
+			$('td#car2Score').text(car2Score);
+			// function for showing winner
+			gameReset();
+		}
+	}
 
-	});
+		$(document).keydown(function(e) {
+		    if(e.keyCode == 65){
+		        console.log('A was pressed') ;
+				$('#car1').remove() ;
+				currentClickCar1++;
+				$('tr.1').append("<td class='newTD'><img id='car1' src=" + car1 + "></td>");       // this works.......
+				winner();
+
+		    } else if (e.keyCode == 76) {
+		        console.log('L was pressed');
+		        $('#car2').remove() ;
+		        currentClickCar2++;
+		        $('tr.2').append("<td class='newTD'><img id='car2' src=" + car2 + "></td>");       // this works.......  
+			   	winner();
+		    }
+		});
 });
 
 
